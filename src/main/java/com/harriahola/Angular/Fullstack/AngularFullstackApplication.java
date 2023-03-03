dbbe663dd8563b5085056e3692cce38826a13e3e
crowelian-w <harricgi@gmail.com>
2023-03-03 09:28:34 +0200

    Initialized spring boot

---
+++ src/main/java/com/harriahola/Angular/Fullstack/AngularFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.harriahola.Angular.Fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularFullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harriahola/Angular/Fullstack/AngularFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harriahola.Angular.Fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
