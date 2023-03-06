a8d32321d3a6c739eaa074f5579493df82a61105
crowelian-w <harricgi@gmail.com>
2023-03-06 13:24:22 +0200

    format list.components.ts and import ReactiveFormsModule

--- frontend/src/app/anti-hero/pages/list/list.component.ts
+++ frontend/src/app/anti-hero/pages/list/list.component.ts
@@ -4,28 +4,32 @@ import { AntiHero } from '../../models/anti-hero.interface';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
   // sample data of anti hero
   antiHeroes: AntiHero[] = [
     {
-      firstName: "Eddie",
-      lastName: "Brock",
-      house: "New York",
-      knownAs: "Venom"
-    }
-  ]
-  headers: {headerName: string, fieldName: keyof AntiHero}[] = [
-    {headerName: "First Name", fieldName: "firstName"},
-    {headerName: "Last Name", fieldName: "lastName"},
-    {headerName: "House", fieldName: "house"},
-    {headerName: "Known As", fieldName: "knownAs"},
-  ]
+      firstName: 'Eddie',
+      lastName: 'Brock',
+      house: 'New York',
+      knownAs: 'Venom',
+    },
+    {
+      firstName: 'Eddie 2',
+      lastName: 'Brock 2',
+      house: 'New York 2',
+      knownAs: 'Venom 2',
+    },
+  ];
+  headers: { headerName: string; fieldName: keyof AntiHero }[] = [
+    { headerName: 'First Name', fieldName: 'firstName' },
+    { headerName: 'Last Name', fieldName: 'lastName' },
+    { headerName: 'House', fieldName: 'house' },
+    { headerName: 'Known As', fieldName: 'knownAs' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
